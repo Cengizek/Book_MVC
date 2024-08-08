f87d8bcd9141370edb7fcb773addce52e0187320
Cengizek <ekmekcicengiz64.com>
2024-08-08 19:46:47 +0300

    first commit

---
+++ WebApplication1.DataAccess/Repository/IRepository/IUnitofWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.DataAccess.Repository.IRepository
+{
+    public interface IUnitofWork
+    {
+        ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
+        ICompanyRepository Company { get; }
+        IApplicationUserRepository ApplicationUser { get; }
+        IShoppingCartRepository ShoppingCart { get; }
+        IOrderDetailRepository OrderDetail { get; }
+        IOrderHeaderRepository OrderHeader { get; }
+        void Save();
+    }
+}
